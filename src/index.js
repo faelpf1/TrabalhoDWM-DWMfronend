8652a2428c17a585efa3722ecba119c08596b3e5
Rafael de Paula Filgueiras <rafaelpfilgueiras@gmail.com>
2021-03-21 22:39:19 +0000

    Adionados os packages que faltavam e o axios configurado

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+//import 'bootstrap/dist/css/bootstrap.min.css';
 //import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+    <Switch>
+        <Route path="/" exact={true} component={App} />
+        //<Route path="/cadastrar" component={Cadastro} />
+        /*<Route path="/listar" component={Lista} />
+    </Switch>
+    </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
